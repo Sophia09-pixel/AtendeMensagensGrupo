5543494927b95dcadae0092c9cb80754055251bd
Sophia09-pixel <68878547+Sophia09-pixel@users.noreply.github.com>
2024-04-16 16:17:26 -0300

    Add files via upload

--- Cp2/src/aplicacao/AtendimentoMensagem.java
+++ Cp2/src/aplicacao/AtendimentoMensagem.java
@@ -4,13 +4,17 @@ public class AtendimentoMensagem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Aluno: Livia Gallafrio
-		//Aluno: Leonardo Mazzuca
-		//Aluno: Luis Rodrigues
-		//Aluno: Sophia de Sousa
-		//Aluno: Victor Yudi
+		//Aluno: Livia Gallafrio rm: 99508
+		//Aluno: Leonardo Mazzuca rm:99430 
+		//Aluno: Luis Rodrigues rm: 551234
+		//Aluno: Sophia de Sousa rm: 551442
+		//Aluno: Victor Yudi rm: 98046
+		
+		int opcao=0;
+		do {
 		
 		
+		} while (opcao!=3);
 
 	}
 
--- Cp2/src/filas/FilaMensagem.java
+++ Cp2/src/filas/FilaMensagem.java
@@ -2,4 +2,37 @@ package filas;
 
 public class FilaMensagem {
 
+	public final int N = 3;
+	String dados[] = new String[N];
+	int ini, fim, cont;
+
+	public void init() {
+		ini = fim = cont = 0;
+	}
+	public boolean isEmpty() {
+		return (cont==0);
+	}
+	public boolean isFull() {
+		return (cont==N);
+	}
+	public void enqueue(String elem) {
+		if (isFull())
+			System.out.println("Fila cheia");
+		else {
+			dados[fim] = elem;
+			fim = (fim+1) % N;
+			cont++;
+		}
+	}
+	public String dequeue() {
+		String elem = dados[ini];
+		ini = (ini+1) % N;
+		cont--;
+		return elem;
+	}
+	
+	public String first() {
+		return dados[ini];
+	}
+	
 }
